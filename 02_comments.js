dedc2d42fa356193cdc6ac66bff4e6650a6746ca
Demi god <moneshram4@gmail.com>
2026-02-21 22:08:02 +0530

    Day 1

---
+++ 02_comments.js
@@ -0,0 +1,18 @@
+console.log("This is my first JavaScript code");
+// This is a single line comment
+console.log("This is a single line comment");
+
+console.log("This is a multi line comment");
+/*
+This is a multi line comment
+This is a multi line comment
+This is a multi line comment
+This is a multi line comment
+*/ 
+console.log("This is a multi line comment"); 
+
+// There is a shortcut to comment and uncomment a line of code in VS Code
+// Windows: Ctrl + /
+
+// There is also a shortcut to comment and uncomment multiple lines of code in VS Code
+// Windows: Shift + Alt + A
