9df4ea43697c4a104cc073014a4f0d754a2b4592
Demi god <moneshram4@gmail.com>
2026-02-21 22:09:08 +0530

    Day 1

---
+++ 04_constants.js
@@ -0,0 +1,23 @@
+// constants in JavaScript
+// A constant is a variable that cannot be reassigned after it has been initialized. 
+// In JavaScript, you can declare a constant using the const keyword.
+//  Constants are block-scoped and must be initialized at the time of declaration.
+
+// declaring and initializing a constant
+const PI = 3.14159;
+console.log(PI);
+// trying to reassign a constant will result in an error
+
+// hardcoded value
+/* 
+hardcoded values are values that are directly written into the code, rather than being stored in a variable or constant.
+They are often used for values that are unlikely to change, such as mathematical constants (e.g., PI) or configuration settings. 
+ However, hardcoding values can make code less flexible and harder to maintain, as changes to the value would require modifying the code itself.
+*/ 
+const GRAVITY = 9.81;
+console.log(GRAVITY);
+
+const age =22;
+const YEAR_OF_BIRTH = 2004;
+
+console.log(age, YEAR_OF_BIRTH);
